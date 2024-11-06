338aba885a4d3391b102219fc133dc3068481926
alhussein <husain.f4l@gmail.com>
2024-11-06 15:09:38 +0300

    gixat

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: 'http://localhost:4200',  // Allow your Angular app's origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
-
-
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  app.enableCors({});
+  await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
